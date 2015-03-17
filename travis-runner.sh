e9a7673ec45ce0654c494df68172b8e8a94fd1ba
Sam Saccone <samccone@gmail.com>
2015-03-16 20:12:14 -0400

    work

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" != "true" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site > /dev/null 2>&1
   cd site
@@ -16,8 +16,11 @@ then
   # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
   git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages > /dev/null 2>&1
 else
-  git remote add real git@github.com:tastejs/todomvc.git && \
+  git remote add real https://github.com/tastejs/todomvc.git && \
   git fetch real && \
   cd browser-tests/ && \
-  git diff HEAD real/master --name-only |  awk 'BEGIN {FS = "/"}; {print $1 "/" $2 "/" $3}' | uniq | grep -v \/\/ | grep examples | awk -F '[/]' '{print "--framework=" $2}' | xargs npm run test --
+  npm i && \
+  git diff HEAD real/master --name-only |  awk 'BEGIN {FS = "/"}; {print $1 "/" $2 "/" $3}' | uniq | grep -v \/\/ | grep examples | awk -F '[/]' '{print "--framework=" $2}' | xargs npm run test -- --grep "be hidden when there are no items"
+
+  exit $?
 fi
